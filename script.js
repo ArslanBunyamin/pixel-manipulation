36147f893c6b14311c32ff33354c1ce089d7cc9c
ArslanBunyamin <arslannbunyamin@gmail.com>
2024-06-06 13:49:19 +0300

    resolution added

--- script.js
+++ script.js
@@ -2,53 +2,53 @@ var canvas = document.getElementById("theCanvas");
 var ctx = canvas.getContext("2d", { willReadFrequently: true });
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-var shade = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:," + '"' + "^`'. ";
+var shade =
+  "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:," +
+  '"' +
+  "^`'. ";
 function getPixelBrightness(imageData, x, y) {
-    var index = (y * imageData.width + x) * 4;
-    var red = imageData.data[index];
-    var green = imageData.data[index + 1];
-    var blue = imageData.data[index + 2];
-    return Math.floor((red + green + blue) / 3);
+  var index = (y * imageData.width + x) * 4;
+  var red = imageData.data[index];
+  var green = imageData.data[index + 1];
+  var blue = imageData.data[index + 2];
+  return Math.floor((red + green + blue) / 3);
 }
 function draw() {
-    if (!video.videoHeight)
-        return requestAnimationFrame(draw);
-    if (!ctx)
-        return alert("html canvas desteklenmiyor!");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var videoWidth = video.videoWidth / 12;
-    var videoHeight = video.videoHeight / 12;
-    ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
-    var data = ctx.getImageData(0, 0, videoWidth, videoHeight);
-    // ctx.clearRect(0, 0, videoWidth, videoHeight)
-    var charSize;
-    var paddingleft = 0;
-    if (window.innerHeight < window.innerWidth) {
-        charSize = window.innerHeight / videoHeight;
-        paddingleft = window.innerWidth / 2 - videoWidth * charSize / 2;
+  if (!video.videoHeight) return requestAnimationFrame(draw);
+  if (!ctx) return alert("html canvas desteklenmiyor!");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var resolution = 16;
+  var videoWidth = video.videoWidth / resolution;
+  var videoHeight = video.videoHeight / resolution;
+  ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+  var data = ctx.getImageData(0, 0, videoWidth, videoHeight);
+  // ctx.clearRect(0, 0, videoWidth, videoHeight)
+  var charSize;
+  var paddingleft = 0;
+  if (window.innerHeight < window.innerWidth) {
+    charSize = window.innerHeight / videoHeight;
+    paddingleft = window.innerWidth / 2 - (videoWidth * charSize) / 2;
+  } else {
+    charSize = window.innerWidth / videoWidth;
+    paddingleft = 0;
+  }
+  for (var x = 0; x < data.width; x++) {
+    for (var y = 0; y < data.height; y++) {
+      var brihtness = getPixelBrightness(data, x, y);
+      var shadeIndex = Math.floor((brihtness * shade.length) / 255);
+      ctx.font = charSize + "px Arial";
+      ctx.fillText(shade[shadeIndex], paddingleft + x * charSize, y * charSize);
     }
-    else {
-        charSize = window.innerWidth / videoWidth;
-        paddingleft = 0;
-    }
-    for (var x = 0; x < data.width; x++) {
-        for (var y = 0; y < data.height; y++) {
-            var brihtness = getPixelBrightness(data, x, y);
-            var shadeIndex = Math.floor(brihtness * shade.length / 255);
-            ctx.font = charSize + "px Arial";
-            ctx.fillText(shade[shadeIndex], paddingleft + x * charSize, y * charSize);
-        }
-    }
-    requestAnimationFrame(draw);
+  }
+  requestAnimationFrame(draw);
 }
 requestAnimationFrame(draw);
 // Select the video element
-var video = document.getElementById('webcam');
+var video = document.getElementById("webcam");
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-        .then(function (stream) {
-        if (video) {
-            video.srcObject = stream;
-        }
-    });
+  navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+    if (video) {
+      video.srcObject = stream;
+    }
+  });
 }
